c5e2805ca0be97de8e5b9823677ab76bb17d1b08
Andrey Shindarev <ashindarev@gmail.com>
2024-09-28 00:46:02 +0300

    Change home path definition

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ ETSI_ORG_VERSION=1.2-SNAPSHOT
 ETSI_ORG_JAR="etsiorg-downloader-${ETSI_ORG_VERSION}-jar-with-dependencies.jar"
 LOGBACK_FILE="./src/main/resources/logback.xml"
 if [ ! -f "target/${ETSI_ORG_JAR}" ]; then
-  mvn -Dmaven.repo.local=~/.m2 clean package
+  mvn -Dmaven.repo.local=${HOME}/.m2 clean package
 fi
 java -Dlogback.configurationFile=${LOGBACK_FILE} -jar target/${ETSI_ORG_JAR}
